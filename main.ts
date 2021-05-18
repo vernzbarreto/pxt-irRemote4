f37fd98d191d3c5f9a3e8e8946e7fd1ab3372792
joey@emakefun.com <joey@emakefun.com>
2021-05-18 19:54:43 +0800

    update irpin

--- main.ts
+++ main.ts
@@ -51,6 +51,29 @@ enum RemoteButton {
     NUM9 = 0x4A
 };
 
+ enum  IrPins{
+
+     P0=  3,
+     P1=  2,
+     P2=  1,
+     P3=  4,
+     P4=  5,
+     P5=  17,
+     P6=  12,
+     P7=  11,
+     P8=  18,
+     P9=  10,
+     P10= 6,
+     P11= 26,
+     P12= 20,
+     P13= 23,
+     P14= 22,
+     P15= 21,
+     P16= 16,
+     P19= 0,
+     P20= 30
+   };
+
 //% color="#EE6A50" weight=10 icon="\uf013"
 namespace magicbit {
     /**
