af8c503b9d4f8b2c0a56d0492241d1bc57c8ab9a
xiaozhou-lei <61865102+xiaozhou-lei@users.noreply.github.com>
2021-09-14 18:56:47 +0800

    update ir microbic v1 v2 blocks

--- Remote.ts
+++ Remote.ts
@@ -145,7 +145,7 @@ namespace Remote {
      * button pushed.
      */
     //% blockId=onPressEvent
-    //% block="当红外遥控器按键|%btn|按下" shim=IrRemote::onPressEvent group="micro:bit(v1)"
+    //% block="当红外遥控器按键|%btn|按下" shim=IrRemote::onPressEvent
     export function OnPressEvent(btn: RemoteButton, body: () => void): void {
         return;
     }
@@ -154,7 +154,7 @@ namespace Remote {
      * button pushed.
      */
     //% blockId=onPressEvent_EM
-    //% block="当EM红外遥控器按键|%btn|按下" shim=IrRemote::onPressEvent_EM group="micro:bit(v1)"
+    //% block="当EM红外遥控器按键|%btn|按下" shim=IrRemote::onPressEvent_EM
     export function OnPressEvent_EM(btn: EM_RemoteButton, body: () => void): void {
         return;
     }
@@ -164,24 +164,12 @@ namespace Remote {
      *  @param pin describe parameter here, eg: IrPins.P5  
      */
     //% blockId=IrRemote_init 
-    //% block="红外遥控器初始化引脚|%pin %mode" shim=IrRemote::IrRemote_init group="micro:bit(v1)"
+    //% block="Micro:bit V1 红外遥控器初始化引脚|%pin 遥控器 %mode" shim=IrRemote::IrRemote_init 
     export function IrRemote_init(pin: IrPins, mode: REMOTE_MODE): void {
         return;
     }
     
-    
-    
-    
-    
-export class Packeta {
-    public mye: string;
-    public myparam: number;
-}
-
-
-let irstate:string;
-let state:number;
- /**
+    /**
  * Read IR sensor value V2.
  */
 	
@@ -190,74 +178,86 @@ let state:number;
      *  @param pin describe parameter here, eg: IrPins.P5  
      */
     //% blockId=IrRemote_init_V2 
-    //% block="红外遥控器初始化引脚|%pin %mode" shim=IrRemote::IrRemote_init_V2 group="micro:bit(v2)"
+    //% block="Micro:bit V2 红外遥控器初始化引脚|%pin 遥控器 %mode" shim=IrRemote::IrRemote_init_V2 
     export function IrRemote_init_V2(pin: V2Pins, mode: REMOTE_MODE): void {
         return;
     }
-	
-//% advanced=true shim=maqueenIRV2::irCode
-function irCode(): number {
-    return 0;
-}
-
-//% weight=5
-//% group="micro:bit(v2)"
-//% blockId=IR_readv2 block="红外接收的值"
-export function IR_readV2(): string {
-    pins.setPull(DigitalPin.P1, PinPullMode.PullUp);
-    let val = valuotokeyConversion();
-    return val;
-}
-
-//% weight=2
-//% group="micro:bit(v2)"
-//% blockId=IR_callbackUserv2 block="当接收到红外时运行"
-//% draggableParameters
-export function IR_callbackUserV2(cb: (message: string) => void) {
-    pins.setPull(DigitalPin.P1, PinPullMode.PullUp);
-    state = 1;
-    control.onEvent(11, 22, function() {
-        cb(irstate)
-    }) 
-}
-
-function valuotokeyConversion(): string {
-    // let irdata: number;
-	let str: string;
-    // basic.showString("" + (irCode()))
-    switch (irCode()) {
-        case 0xe916: str = '*'; break;
-        case 0xf20d: str = '#'; break;
-        case 0xe718: str = 'UP'; break;
-        case 0xf708: str = 'LEFT'; break;
-        case 0xe31c: str = 'OK'; break;
-        case 0xA55A: str = 'RIGHT'; break;
-        case 0xe619: str = '0'; break;
-        case 0xad52: str = 'DOWN'; break;
-        case 0xba45: str = '1'; break;
-        case 0xb946: str = '2'; break;
-        case 0xb847: str = '3'; break;
-        case 0xbb44: str = '4'; break;
-        case 0xbf40: str = '5'; break;
-        case 0xbc47: str = '6'; break;
-        case 0xf807: str = '7'; break;
-        case 0xea15: str = '8'; break;
-        case 0xf609: str = '9'; break;
-        default: str = ''; break;
-    }
-    return str;
-}
-
-basic.forever(() => {
-    if(state == 1){
-        irstate = IR_readV2();
-        if(irstate != '-1'){
-            control.raiseEvent(11, 22)
-        }
-    }
     
-    basic.pause(20);
-})
+    
+    
+// export class Packeta {
+//     public mye: string;
+//     public myparam: number;
+// }
+
+
+// let irstate:string;
+// let state:number;
+ 
+	
+// //% advanced=true shim=maqueenIRV2::irCode
+// function irCode(): number {
+//     return 0;
+// }
+
+// //% weight=5
+// //% group="micro:bit(v2)"
+// //% blockId=IR_readv2 block="红外接收的值"
+// export function IR_readV2(): string {
+//     pins.setPull(DigitalPin.P1, PinPullMode.PullUp);
+//     let val = valuotokeyConversion();
+//     return val;
+// }
+
+// //% weight=2
+// //% group="micro:bit(v2)"
+// //% blockId=IR_callbackUserv2 block="当接收到红外时运行"
+// //% draggableParameters
+// export function IR_callbackUserV2(cb: (message: string) => void) {
+//     pins.setPull(DigitalPin.P1, PinPullMode.PullUp);
+//     state = 1;
+//     control.onEvent(11, 22, function() {
+//         cb(irstate)
+//     }) 
+// }
+
+// function valuotokeyConversion(): string {
+//     // let irdata: number;
+// 	let str: string;
+//     // basic.showString("" + (irCode()))
+//     switch (irCode()) {
+//         case 0xe916: str = '*'; break;
+//         case 0xf20d: str = '#'; break;
+//         case 0xe718: str = 'UP'; break;
+//         case 0xf708: str = 'LEFT'; break;
+//         case 0xe31c: str = 'OK'; break;
+//         case 0xA55A: str = 'RIGHT'; break;
+//         case 0xe619: str = '0'; break;
+//         case 0xad52: str = 'DOWN'; break;
+//         case 0xba45: str = '1'; break;
+//         case 0xb946: str = '2'; break;
+//         case 0xb847: str = '3'; break;
+//         case 0xbb44: str = '4'; break;
+//         case 0xbf40: str = '5'; break;
+//         case 0xbc47: str = '6'; break;
+//         case 0xf807: str = '7'; break;
+//         case 0xea15: str = '8'; break;
+//         case 0xf609: str = '9'; break;
+//         default: str = ''; break;
+//     }
+//     return str;
+// }
+
+// basic.forever(() => {
+//     if(state == 1){
+//         irstate = IR_readV2();
+//         if(irstate != '-1'){
+//             control.raiseEvent(11, 22)
+//         }
+//     }
+    
+//     basic.pause(20);
+// })
 
 
 }
