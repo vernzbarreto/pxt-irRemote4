f71274a84297b6062ce03ffe93bc7ee52b7dfc96
Buyuliang <63707976+Buyuliang@users.noreply.github.com>
2021-09-14 16:42:16 +0800

    Update ir.cpp

--- ir.cpp
+++ ir.cpp
@@ -3,7 +3,8 @@
 #include <vector>
 #include "ReceiverIR.h"
 using namespace pxt;
-typedef vector<Action> vA;
+typedef vector<Action> vA; 
+typedef vector<Action> EM_vA;
 
 enum class Pins{
   P0=  3,
@@ -27,7 +28,7 @@ enum class Pins{
   P20= 30
 };
 
-enum class RemoteButton {
+enum class EM_RemoteButton {
       //% block=A
       A = 0x45,
       //% block=B
@@ -72,11 +73,51 @@ enum class RemoteButton {
 	  NUM9 = 0x4A
 };
 
+enum RemoteButton {
+    //% block=1
+    NUM1 = 0x20,
+    //% block=2
+    NUM2 = 0x46,
+    //% block=3
+    NUM3 = 0x47,
+    //% block=4
+    NUM4 = 0x44,
+    //% block=5
+    NUM5 = 0x40,
+    //% block=6
+    NUM6 = 0x43,
+    //% block=7
+    NUM7 = 0x07,
+    //% block=8
+    NUM8 = 0x15,
+    //% block=9
+    NUM9 = 0x09,
+    //% block=*
+    Star = 0x16,
+    //% block=0
+    NUM0 = 0x19,
+    //% block=C
+    Well = 0x0d,
+    //% block=UP
+    UP = 0x18,
+    //% block=LEFT
+    Left = 0x08,
+    //% block=OK
+    Ok = 0x1c,
+    //% block=RIGHT
+    Right = 0x5a,
+    //% block=DOWN
+    Down = 0x52,
+};
+
+
 //% color=50 weight=19 icon="\uf1eb"
 //% block="IrRemote"
 namespace IrRemote { 
   map<RemoteButton, vA> actions;
   map<RemoteButton, uint32_t> lastact;
+  map<EM_RemoteButton, EM_vA> EM_actions;
+  map<EM_RemoteButton, uint32_t> EM_lastact;
   Timer tsb; 
   uint8_t buf[32];
   uint32_t now;
@@ -91,8 +132,17 @@ namespace IrRemote {
     //if(actions.find(btn) == actions.end()) actions[btn] = new vector();
     actions[btn].push_back(body);
   }
+	
+ /**
+  * button pushed.
+  */
+  //%
+  void onPressEvent_EM(EM_RemoteButton btn, EM_Action body) {
+    EM_actions[btn].push_back(body);
+  }
 
   void cA(vA runner){for(int i=0;i<runner.size();i++){runAction0(runner[i]);} }
+  void EM_cA(EM_vA runner){for(int i=0;i<runner.size();i++){runAction0(runner[i]);} }
 
   void onReceivable(){
     int x = rx->getData(&fmt, buf, 32 * 8);
@@ -105,23 +155,47 @@ namespace IrRemote {
     lastact[(RemoteButton)buf[2]] = now;
     cA(actions[(RemoteButton)buf[2]]); 
   }
+	
+  void onReceivable_EM(){
+    int x = rx->getData(&fmt, buf, 32 * 8);
+	  uBit.serial.send("buf[2]: ");
+	  uBit.serial.send(buf[2]);
+	  uBit.serial.send("\r\n");
+    if(EM_actions.find((EM_RemoteButton)buf[2]) == EM_actions.end()) return;
+    now = tsb.read_ms();
+    if(now - lastact[(EM_RemoteButton)buf[2]] < 100) return;
+    EM_lastact[(EM_RemoteButton)buf[2]] = now;
+    EM_cA(EM_actions[(EM_RemoteButton)buf[2]]); 
+  }
+	
 
   void monitorIR(){
     while(1){
       while(rx->getState() != ReceiverIR::Received){ 
         uBit.sleep(50);
       }
-      onReceivable();
-    }
+        onReceivable();
   }
 
+  void monitorIR_EM(){
+    while(1){
+      while(rx->getState() != ReceiverIR::Received){ 
+        uBit.sleep(50);
+      }
+        onReceivable_EM();
+  }
+	  
   /**
   * initialises local variablesssss
   */
   //%
-  void IrRemote_init(Pins pin){
+  void IrRemote_init(Pins pin, MODE mode){
     rx = new ReceiverIR((PinName)pin);
     tsb.start(); //interrupt timer for debounce
-    create_fiber(monitorIR);
+    if (mode == 0) {
+        create_fiber(monitorIR);
+    }elde if (mode == 1) {
+       create_fiber(monitorIR_EM);
+    }
   }
 }
